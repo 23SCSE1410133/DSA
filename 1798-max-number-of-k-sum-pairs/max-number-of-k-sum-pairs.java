01c5347e8cabd2b8db0e5e6e206d2bd217a2492a
Anushka Sharma <anushkasharma09812@gmail.com>
2025-12-24 12:01:37 +0530

    Time: 23 ms (84.48%) | Memory: 69.1 MB (59.65%) - LeetSync

---
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int l=0,r=nums.length-1,ans=0;
+        while(l<r){
+            int sum=nums[l]+nums[r];
+            if(sum==k){
+                ans++;
+                l++;
+                r--;
+            }else if(sum<k){
+                l++;
+            }else{
+                r--;
+            }
+            
+        }
+        return ans;
+    }
+}
