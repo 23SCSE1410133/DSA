ba1c9129633fd301db14e4977d3dba35a64ef78a
Anushka Sharma <anushkasharma09812@gmail.com>
2026-01-08 08:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (7.18%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,13 +2,12 @@ class Solution {
     public int rob(int[] nums) {
         if(nums.length==0) return 0;
         if(nums.length==1) return nums[0];
-        int prev2 = nums[0];
-        int prev1 = Math.max(nums[0], nums[1]);
+        int[] dp=new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
         for(int i=2;i<nums.length;i++){
-            int curr = Math.max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
-        }      
-        return prev1;  
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.length-1];
     }
 }
